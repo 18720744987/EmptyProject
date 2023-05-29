571bb3a4682d7201155d3bf345d45c7d76bca7c2
ZQP <1490148364@qq.com>
2023-05-29 23:39:52 +0800

    第三次push

--- test-project/src/main/java/com/mark/TestStudent.java
+++ test-project/src/main/java/com/mark/TestStudent.java
@@ -4,5 +4,7 @@ public class TestStudent {
     public static void main(String[] args) {
         Student student = new Student();
         student.setAge("10");
+
+        System.out.println(student.getAge());
     }
 }
